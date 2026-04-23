f492467ac5d114aff2e2d5c6baff060d969b60d1
SaurabhMV <mdsaurabhy2k@gmail.com>
2026-04-23 13:22:31 -0400

    Update app.py

--- app.py
+++ app.py
@@ -242,7 +242,7 @@ if st.session_state.results_data:
             rows=3, cols=1, 
             shared_xaxes=True, 
             vertical_spacing=0.03, 
-            row_heights=[0.35, 0.35, 0.30],
+            row_heights=[0.20, 0.40, 0.40],
             specs=[[{"secondary_y": False}], [{"secondary_y": True}], [{"secondary_y": False}]] # Row 2 gets secondary Y
         )
 
